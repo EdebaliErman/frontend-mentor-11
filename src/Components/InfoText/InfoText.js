b81010e2be4340feac3c870c7a2696e0e1448a86
EdebaliErman <edebalierman@gmail.com>
2023-11-17 03:34:05 +0300

    Finished

--- src/Components/InfoText/InfoText.js
+++ src/Components/InfoText/InfoText.js
@@ -2,9 +2,9 @@ import React from 'react'
 
 function InfoText() {
   return (
-    <div className='w-1/3'>
-      <h1 className='text-3xl my-6 font-sans  font-bold text-[#3f2552]'>Simple booking </h1>
-      <h5 className='text-2xl text-gray-500 opacity-40'>Stay in touch with our dog walkers through the chat interface. This makes it easy to discuss arrangements and make bookings. Once the walk has been completed you can rate your walker and book again all through the chat </h5>
+    <div className='w-1/3 mb-28'>
+      <h1 className='text-6xl my-12 font-sans  font-bold text-[#3f2552]'>Simple booking </h1>
+      <h5 className='text-2xl text-gray-500 opacity-60'>Stay in touch with our dog walkers through the chat interface. This makes it easy to discuss arrangements and make bookings. Once the walk has been completed you can rate your walker and book again all through the chat </h5>
     </div>
   )
 }
--- src/Components/SvgBack/SvgBack.js
+++ src/Components/SvgBack/SvgBack.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 function SvgBack() {
-    return <svg className='z-10 bg-gradient-to-t from-[#913aff] to-[#c640ff] w-[23.2%] h-[85%] absolute top-0 left-0 rounded-b-[18rem]'></svg>
+    return <svg className='z-10 bg-gradient-to-t from-[#913aff] to-[#c640ff] w-[25.00%] h-[85%] absolute top-0 left-0 rounded-b-[19.8rem]'></svg>
 }
 
 export default SvgBack
