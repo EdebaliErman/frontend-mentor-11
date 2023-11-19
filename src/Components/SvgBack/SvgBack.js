8dd02b01fe778468ec30fd95d1428d2e28031bb1
EdebaliErman <edebalierman@gmail.com>
2023-11-19 15:06:16 +0300

    Svg css

--- src/Components/SvgBack/SvgBack.js
+++ src/Components/SvgBack/SvgBack.js
@@ -1,7 +1,7 @@
 import React from 'react'
-
+import "./SvgBack.css"
 function SvgBack() {
-    return <svg className='z-10 bg-gradient-to-t from-[#913aff] to-[#c640ff] w-[25.00%] h-[85%] absolute top-0 left-0 rounded-b-[19.8rem]'></svg>
+    return <svg className='svgback'></svg>
 }
 
 export default SvgBack
